c3b8b743a56b9b2a7108d09b628a7e71ea483dbf
Nicholas Steele <Nick@new-host-3.home>
2012-08-15 14:01:41 -0400

    This is my app to learn rails and talk about ales

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlesOnRails::Application.config.secret_token = 'c7132b01b6fab5284ad8563960e28579f278f338a3ec5bc22e72944326b9d6be7bb682845d9b79b986e1f79280ac074ff69885223c608fd73ef337936edeab60'
